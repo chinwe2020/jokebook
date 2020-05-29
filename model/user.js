d94f755529b3c9e9e86b7472f8b043e0cd05cc9f
Chinwe Watkins <chinwewatkins@chinwes-mbp.attlocal.net>
2020-05-29 16:51:14 -0500

    begin setup

---
+++ model/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = ({
+    _id:
+    name:
+    email:
+    password:
+});
+
+module.exports = mongoose.model('User', userSchema);
