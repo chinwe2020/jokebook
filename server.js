459b36bbe971049d946601b07da535a33da83aa2
Chinwe Watkins <chinwewatkins@chinwes-mbp.attlocal.net>
2020-05-29 19:25:12 -0500

    oauth1-6

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override')
 
-require('dotenv').config() 
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+require('./config/database');
+require('./config/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +24,16 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({
+  secret: 'ChinweIsAwesome!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
