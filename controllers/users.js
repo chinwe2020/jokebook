0c6758f3c33d1483702fad3d03fbe8c82049850b
Chinwe Watkins <chinwewatkins@chinwes-mbp.attlocal.net>
2020-06-01 09:18:58 -0500

    update views

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,8 @@
 const User = require('../models/user');
 
 module.exports = {
-    index
+    index,
+    
 };
 
 function index (req,res) {
--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/jokebook', function(req, res, next) {
+  res.render('Home', { title: 'Home Page' });
 });
 
 module.exports = router;
